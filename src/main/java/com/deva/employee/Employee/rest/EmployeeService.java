ffc0d1ca1b7d3744e0ffa9c103ea6c692dc61cd4
Deva Raju Polasi <96421029+devarajup@users.noreply.github.com>
2021-12-28 11:45:29 +0530

    commit

--- src/main/java/com/deva/employee/Employee/rest/EmployeeService.java
+++ src/main/java/com/deva/employee/Employee/rest/EmployeeService.java
@@ -1,24 +1,34 @@
 package com.deva.employee.Employee.rest;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class EmployeeService {
-    Map<String,String> map = new HashMap<>();
-    public void addEmployee(String id,String name){
-        map.put(id,name);
+    Map<String, String> map = new HashMap<>();
+
+    public void addEmployee(String id, String name) {
+        map.put(id, name);
     }
-    public  String   getEmployee(String id){
-        return  map.get(id);
+
+    public String getEmployee(String id) {
+        return map.get(id);
     }
-    public Map<String, String> getAllEmployee(){
-        return  map;
+
+    public Map<String, String> getAllEmployee() {
+        return map;
     }
-    public void  deleteEmployee(String id){
+
+    public void deleteEmployee(String id) {
         map.remove(id);
+    }
+    public  void patchEmployee(String id,String oldValue,String name){
+        map.replace(id, oldValue,name);
+    }
+    public  void  putMapping(String id, String newId ,String name){
+        map.remove(id);
+        map.put(newId,name);
     }
 }
--- src/main/java/com/deva/employee/Employee/rest/rest.java
+++ src/main/java/com/deva/employee/Employee/rest/rest.java
@@ -2,30 +2,47 @@ package com.deva.employee.Employee.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
 
 @RestController
 public class rest {
     @Autowired
-  private   EmployeeService service;
+    private EmployeeService service;
 
     @PostMapping("/add/{id}/{name}")
-    public void addEmployee(@PathVariable String id,
-                            @PathVariable String name){
-        service.addEmployee(id,name);
-    }
-    @GetMapping("/getone/{id}")
-    public String getEmployee(@PathVariable String id){
-       return  service.getEmployee(id);
-    }
-    @GetMapping("/getall")
-    public Map<String, String> getAllEmployee(){
-       return  service.getAllEmployee();
-    }
-    @DeleteMapping("/delete/{id}")
-    public void deleteEmployee(@PathVariable String id){
-service.deleteEmployee(id);
+    public String  addEmployee(@PathVariable String id,
+                            @PathVariable String name) {
+        service.addEmployee(id, name);
+        return  id+" and "+name+" added";
     }
 
+    @GetMapping("/getone/{id}")
+    public String getEmployee(@PathVariable String id) {
+        return service.getEmployee(id);
+    }
+
+    @GetMapping("/getall")
+    public Map<String, String> getAllEmployee() {
+        return service.getAllEmployee();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteEmployee(@PathVariable String id) {
+        service.deleteEmployee(id);
+        return  id+"deleted";
+    }
+    @PatchMapping("/patch/{id}/{oldName}/{newName}")
+    public String patchEmployee(@PathVariable String id,
+                                @PathVariable String oldName,
+                                @PathVariable String newName){
+            service.patchEmployee(id,oldName,newName);
+        return  id+" replaced or updated";
+    }
+    @PutMapping("/put/{id}/{newId}/{name}")
+    public String putMapping(@PathVariable String id,
+                             @PathVariable String newId,
+                             @PathVariable String name){
+        service.putMapping(id,newId,name);
+        return "replaced successfully";
+    }
 }
